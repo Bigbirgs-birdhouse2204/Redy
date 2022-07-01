8fce99cdb90ed7fd457f9ba5a8c73b22d2a36955
Nikul Patel <nikulp1997@gmail.com>
2022-07-01 16:47:46 -0400

    Sign up User

--- App.js
+++ App.js
@@ -1,31 +1,29 @@
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "./app/Screens/Home";
-import Maps from "./app/Screens/Maps";
-import SignInScreen from "./app/Screens/SignInScreen";
-import SignUpScreen from "./app/Screens/SignUpScreen";
-
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './app/Screens/Home';
+import Maps from './app/Screens/Maps';
+import SignInScreen from './app/Screens/SignInScreen';
+import SignUpScreen from './app/Screens/SignUpScreen';
+import SignUpUser from './app/Screens/SignUpUser';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      {/* <Stack.Screen name="Home" component={Home} /> */}
-      <Stack.Screen name="Sign In" component={SignInScreen} />
-      <Stack.Screen name="Sign Up" component={SignUpScreen} />
-
+        {/* <Stack.Screen name="Home" component={Home} /> */}
+        {/* <Stack.Screen name="Sign In" component={SignInScreen} /> */}
+        <Stack.Screen name="Sign Up" component={SignUpUser} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-  // const globalScreenOptions = {
-  //   headerStyle: { backgroundColor: colors.secondaryColor },
-  //   headerTitleStyle: { color: colors.white },
-  //   headerTintColor: colors.white,
-  // };
+// const globalScreenOptions = {
+//   headerStyle: { backgroundColor: colors.secondaryColor },
+//   headerTitleStyle: { color: colors.white },
+//   headerTintColor: colors.white,
+// };
