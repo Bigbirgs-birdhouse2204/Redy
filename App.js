1e92fab17cf99f7104ee69c11ff75f00af5eef9e
navis6927 <100895918+navis6927@users.noreply.github.com>
2022-07-01 16:21:42 -0400

    Sign In Screen (P1)

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./app/Screens/Home";
 import Maps from "./app/Screens/Maps";
+import SignInScreen from "./app/Screens/SignInScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,7 +13,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="Home" component={Maps} />
+      <Stack.Screen name="Home" component={SignInScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
