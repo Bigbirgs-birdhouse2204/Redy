ac647de553cf4ead4fd3ce5bd938db148bcfaabd
Simon <simon.jin.chen@gmail.com>
2022-07-07 15:28:18 -0400

    Merge branch 'main' of github.com:Bigbirgs-birdhouse2204/Redy

--- App.js
+++ App.js
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import Home from './app/Screens/Home';
-import Maps from './app/Screens/Maps';
-import SignInScreen from './app/Screens/SignInScreen';
-import SignUpScreen from './app/Screens/SignUpScreen';
-import SignUpUser from './app/Screens/SignUpUser';
-import SignUpBusiness from './app/Screens/SignUpBusiness';
-import { Provider } from 'react-redux';
-import store from './app/store';
-import { createStackNavigator } from 'react-navigation-stack';
-import SignIn from './app/Screens/SignInScreen';
-import Settings from './app/Screens/settings';
-=======
 import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -26,8 +9,6 @@ import SignUpScreen from "./app/Screens/SignUpScreen";
 import SingleRestaurant from "./app/Screens/SingleRestaurant";
 import SignUpUser from "./app/Screens/SignUpUser";
 
->>>>>>> bceff68487bf88ed634e7143b1bfc26e2447dc08
-
 const Stack = createNativeStackNavigator();
 // const AuthStack = createStackNavigator({
 //   SignIn: SignIn,
@@ -37,29 +18,14 @@ const Stack = createNativeStackNavigator();
 // });
 export default function App() {
   return (
-<<<<<<< HEAD
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Sign In" component={SignInScreen} />
-          <Stack.Screen name="Choose Account" component={SignUpScreen} />
-          <Stack.Screen name="Sign Up User" component={SignUpUser} />
-          <Stack.Screen name="Sign Up Business" component={SignUpBusiness} />
-          <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
-=======
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="Sign In" component={SignUpUser} />
-      {/* <Stack.Screen name="Sign In" component={Maps} /> */}
-      <Stack.Screen name="Sign Up" component={SignUpScreen} />
-      <Stack.Screen name="Single Restaurant" component={SingleRestaurant} />
-
+        <Stack.Screen name="Sign In" component={SignUpUser} />
+        {/* <Stack.Screen name="Sign In" component={Maps} /> */}
+        <Stack.Screen name="Sign Up" component={SignUpScreen} />
+        <Stack.Screen name="Single Restaurant" component={SingleRestaurant} />
       </Stack.Navigator>
     </NavigationContainer>
->>>>>>> bceff68487bf88ed634e7143b1bfc26e2447dc08
   );
 }
 
